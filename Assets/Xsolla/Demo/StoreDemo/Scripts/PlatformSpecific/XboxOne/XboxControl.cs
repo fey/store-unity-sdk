b010ee2c6fd05de9386821d79ce35bf00f6d4771
MaximLevin <m.levin@xsolla.com>
2019-10-04 08:26:51 +0500

    Split control and item select logic

---
+++ Assets/Xsolla/Demo/StoreDemo/Scripts/PlatformSpecific/XboxOne/XboxControl.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Xsolla.Core;
+
+public class XboxControl : MonoBehaviour
+{
+    enum Tab
+    {
+        Store,
+        Inventory
+    };
+    Tab tab;
+
+    GroupsController groupController;
+    ItemsTabControl tabController;
+    ItemsController itemsController;
+
+    XboxInput input;
+    int selectedIndex;
+    IItemSelection selectedItem;
+
+    private void Awake()
+    {
+        selectedItem = null;
+        tab = Tab.Store;
+        input = gameObject.AddComponent<XboxInput>();
+        input.ButtonPressedEvent += Input_ButtonPressedEvent;
+    }
+
+    private void Input_ButtonPressedEvent(XboxInput.MenuButton button)
+    {
+        switch(button)
+        {
+            case XboxInput.MenuButton.GroupDown:
+                {
+                    SelectGroupDown();
+                    break;
+                }
+            case XboxInput.MenuButton.GroupUp:
+                {
+                    SelectGroupUp();
+                    break;
+                }
+            case XboxInput.MenuButton.InventoryTab:
+                {
+                    Unfocus();
+                    tabController.SelectInventoryTab();
+                    tab = Tab.Inventory;
+                    break;
+                }
+            case XboxInput.MenuButton.StoreTab:
+                {
+                    Unfocus();
+                    tabController.SelectStoreTab();
+                    tab = Tab.Store;
+                    break;
+                }
+            case XboxInput.MenuButton.ItemUp:
+                {
+                    if (selectedIndex > 2)
+                        selectedIndex -= 3;
+                    else
+                        selectedIndex = 0;
+                    break;
+                }
+            case XboxInput.MenuButton.ItemDown:
+                {
+                    selectedIndex += 3;
+                    break;
+                }
+            case XboxInput.MenuButton.ItemLeft:
+                {
+                    if(selectedIndex > 0)
+                        selectedIndex--;
+                    break;
+                }
+            case XboxInput.MenuButton.ItemRight:
+                {
+                    selectedIndex++;
+                    break;
+                }
+            default:
+                {
+                    Debug.Log("Unhandled button pressed");
+                    break;
+                }
+        }
+    }
+
+    void Start()
+    {
+        groupController = FindObjectOfType<GroupsController>();
+        tabController = FindObjectOfType<ItemsTabControl>();
+        itemsController = FindObjectOfType<ItemsController>();
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+
+    void Update()
+    {
+        UpdateSelectedItem();
+    }
+    
+    void UpdateSelectedItem()
+    {
+        IGroup group = groupController.GetSelectedGroup();
+        if (group == null)
+            return;
+        string groupId = tab == Tab.Store ? group.Id : Constants.InventoryConatainerName;
+        List<IItemSelection> items = itemsController.GetItemsByGroupId(groupId);
+        if (selectedIndex >= items.Count)
+            selectedIndex = 0;
+
+        IItemSelection item = items.Count > 0 ? items[selectedIndex] : null;
+        if (selectedItem != item)
+        {
+            Unfocus();
+            Focus(item);
+        }
+    }
+
+    IItemSelection Focus(IItemSelection item)
+    {
+        selectedItem = item;
+        selectedItem?.Focus();
+        return selectedItem;
+    }
+
+    void Unfocus()
+    {
+        selectedItem?.Unfocus();
+        selectedItem = null;
+    }
+
+    void GetGroups(ref IGroup previous, ref IGroup selected, ref IGroup next)
+    {
+        selected = previous = next = null;
+
+        List<IGroup> groups = groupController.GetGroups();
+        int count = groups.Count;
+        if (count == 0)
+            return;
+
+        selected = groupController.GetSelectedGroup();
+        int index = groups.IndexOf(selected);
+        previous = (index == 0) ? groups.Last() : groups[index - 1];
+        next = groups[(index + 1) % count];
+    }
+
+    void SelectGroupUp()
+    {
+        IGroup previous, selected, next;
+        previous = selected = next = null;
+        GetGroups(ref previous, ref selected, ref next);
+        if (previous != null)
+        {
+            previous.Select();
+            selectedIndex = 0;
+        }
+    }
+
+    void SelectGroupDown()
+    {
+        IGroup previous, selected, next;
+        previous = selected = next = null;
+        GetGroups(ref previous, ref selected, ref next);
+        if (next != null)
+        {
+            next.Select();
+            selectedIndex = 0;
+        }
+    }
+}
--- Assets/Xsolla/Demo/StoreDemo/Scripts/PlatformSpecific/XboxOne/XboxInput.cs
+++ Assets/Xsolla/Demo/StoreDemo/Scripts/PlatformSpecific/XboxOne/XboxInput.cs
@@ -7,222 +7,101 @@ using Xsolla.Core;
 
 public class XboxInput : MonoBehaviour
 {
-	enum Tab
-	{
-		Store,
-		Inventory
-	};
-	Tab tab;
-
-#if UNITY_XBOX_ONE
-	KeyCode storeTabKey = KeyCode.JoystickButton4;
-#else
-	KeyCode storeTabKey = KeyCode.LeftArrow;
-#endif
-
-#if UNITY_XBOX_ONE
-	KeyCode inventoryTabKey = KeyCode.JoystickButton5;
-#else
-	KeyCode inventoryTabKey = KeyCode.RightArrow;
-#endif
-
-#if UNITY_XBOX_ONE
-	string groupAxis = "Vertical";
-#else
-	string groupAxis = "Vertical";
-#endif
-
-#if UNITY_XBOX_ONE
-	string itemAxisX = "Horizontal2";
-#else
-	string itemAxisX = "Horizontal2";
-#endif
-
-#if UNITY_XBOX_ONE
-	string itemAxisY = "Vertical2";
-#else
-	string itemAxisY = "Vertical2";
-#endif
-
-	GroupsController groupController;
-	ItemsTabControl tabController;
-	ItemsController itemsController;
-
-	int selectedIndex;
-	IItemSelection selectedItem;
-
-	private void Awake()
-	{
-		tab = Tab.Store;
-	}
-	// Start is called before the first frame update
-	void Start()
+    public enum MenuButton
     {
-		selectedItem = null;
-		groupController = FindObjectOfType<GroupsController>();
-		tabController = FindObjectOfType<ItemsTabControl>();
-		itemsController = FindObjectOfType<ItemsController>();
-		StartCoroutine("GroupsCoroutine");
-		StartCoroutine("ItemsCoroutine");
-	}
+        GroupDown,
+        GroupUp,
+        StoreTab,
+        InventoryTab,
+        ItemLeft,
+        ItemRight,
+        ItemDown,
+        ItemUp
+    };
 
-	private void OnDestroy()
-	{
-		StopAllCoroutines();
-	}
+    public delegate void ButtonPressedDelegate(MenuButton button);
+    public event ButtonPressedDelegate ButtonPressedEvent;
 
-	// Update is called once per frame
-	void Update()
+	string groupAxis = "Vertical";
+    string itemAxisX = "Axis4";
+	string itemAxisY = "Axis5";
+    string storeAxis = "Axis9";
+    string inventoryAxis = "Axis10";
+
+    void Start()
     {
-		UpdateSelectedItem();
-		MoveThroughtTabs();
-	}
+        StartCoroutine("ItemControl");
+        StartCoroutine("GroupControl");
+        StartCoroutine("TabControl");
+    }
 
-	IEnumerator GroupsCoroutine()
-	{
-		float timeout = 0.001F;
-		while(true) {
-			yield return new WaitForSeconds(timeout);
-			float v = Input.GetAxis(groupAxis);
-			if (Mathf.Abs(v) > 0.01F) {
-				if (v > 0) {
-					SelectGroupUp();
-					selectedIndex = 0;
-				} else {
-					SelectGroupDown();
-					selectedIndex = 0;
-				}
-				timeout = 0.3F;
-			} else {
-				timeout = 0.001F;
-			}
-		}
-	}
+    IEnumerator ItemControl()
+    {
+        float timeout = 0.01F;
+        while (true)
+        {
+            yield return new WaitForSeconds(timeout);
 
-	IEnumerator ItemsCoroutine()
-	{
-		float timeout = 0.001F;
-		while (true) {
-			yield return new WaitForSeconds(timeout);
-			float h = Input.GetAxis(itemAxisX);
-			float v = Input.GetAxis(itemAxisY);
-			if ((Mathf.Abs(h) > 0.01F) || (Mathf.Abs(v) > 0.01F)) {
-				if(Mathf.Abs(h) > 0.01F) {
-					if (h > 0) {
-						selectedIndex++;
-					} else {
-						if (selectedIndex > 0)
-							selectedIndex--;
-					}
-				}
-				if (Mathf.Abs(v) > 0.01F) {
-					if (v < 0) {
-						selectedIndex += 3;
-					} else {
-						if (selectedIndex > 2)
-							selectedIndex -= 3;
-					}
-				}
-				timeout = 0.3F;
-			} else {
-				timeout = 0.001F;
-			}
-		}
-	}
+            float x = Input.GetAxis(itemAxisX);
+            float y = Input.GetAxis(itemAxisY);
+            float absX = Mathf.Abs(x);
+            float absY = Mathf.Abs(y);
 
-	void MoveThroughtTabs()
-	{
-		if (Input.GetKeyUp(storeTabKey)) {
-			Unfocus();
-			tabController.SelectStoreTab();
-			tab = Tab.Store;
-		}
-		if (Input.GetKeyUp(inventoryTabKey)) {
-			Unfocus();
-			tabController.SelectInventoryTab();
-			tab = Tab.Inventory;
-		}
-	}
+            if ((absX > 0.7F) || (absY > 0.7F))
+            {
+                timeout = 0.3F;
 
-	//void MoveThroughtItems()
-	//{
-	//	if (Input.GetKeyUp(KeyCode.D))
-	//		selectedIndex++;
-	//	if (Input.GetKeyUp(KeyCode.S))
-	//		selectedIndex += 3;
+                if (absX > absY)
+                    ButtonPressedEvent?.Invoke(x > 0 ? MenuButton.ItemRight : MenuButton.ItemLeft);
+                else
+                    ButtonPressedEvent?.Invoke(y > 0 ? MenuButton.ItemDown : MenuButton.ItemUp);
+            }
+            else
+                timeout = 0.01F;
+        }
+    }
 
-	//	if (selectedIndex > 0) {
-	//		if (Input.GetKeyUp(KeyCode.A))
-	//			selectedIndex--;
-	//	}
-	//	if (selectedIndex > 2) {
-	//		if (Input.GetKeyUp(KeyCode.W))
-	//			selectedIndex -= 3;
-	//	}
-	//}
+    IEnumerator GroupControl()
+    {
+        float timeout = 0.01F;
+        while (true)
+        {
+            yield return new WaitForSeconds(timeout);
+            float group = Input.GetAxis(groupAxis);
 
-	void UpdateSelectedItem()
-	{
-		IGroup group = groupController.GetSelectedGroup();
-		if (group == null)
-			return;
-		string groupId = tab == Tab.Store ? group.Id : Constants.InventoryConatainerName;
-		List<IItemSelection> items = itemsController.GetItemsByGroupId(groupId);
-		if (selectedIndex >= items.Count)
-			selectedIndex = 0;
+            if (Mathf.Abs(group) > 0.7F)
+            {
+                ButtonPressedEvent?.Invoke(group > 0 ? MenuButton.GroupDown : MenuButton.GroupUp);
+                timeout = 0.3F;
+            }else
+                timeout = 0.01F;
+        }
+    }
 
-		IItemSelection item = items.Count > 0 ? items[selectedIndex] : null;
-		if (selectedItem != item) {
-			Unfocus();
-			Focus(item);
-		}
-	}
+    IEnumerator TabControl()
+    {
+        float timeout = 0.01F;
+        while (true)
+        {
+            yield return new WaitForSeconds(timeout);
+            float store = Mathf.Abs(Input.GetAxis(storeAxis));
+            float inventory = Mathf.Abs(Input.GetAxis(inventoryAxis));
 
-	IItemSelection Focus(IItemSelection item)
-	{
-		selectedItem = item;
-		selectedItem?.Focus();
-		return selectedItem;
-	}
+            if ((store > 0.7F) || (inventory > 0.7F))
+            {
+                if (store > 0.2F)
+                    ButtonPressedEvent?.Invoke(MenuButton.StoreTab);
+                else
+                    ButtonPressedEvent?.Invoke(MenuButton.InventoryTab);
+                timeout = 0.3F;
+            }
+            else
+                timeout = 0.01F;
+        }
+    }
 
-	void Unfocus()
-	{
-		selectedItem?.Unfocus();
-		selectedItem = null;
-	}
-
-	void GetGroups(out IGroup previous, out IGroup selected, out IGroup next)
-	{
-		selected = previous = next = null;
-
-		List<IGroup> groups = groupController.GetGroups();
-		int count = groups.Count;
-		if (count == 0)
-			return;
-
-		selected = groupController.GetSelectedGroup();
-		int index = groups.IndexOf(selected);
-		previous = (index == 0) ? groups.Last() : groups[index - 1];
-		next = groups[(index + 1) % count];
-	}
-
-	void SelectGroupUp()
-	{
-		IGroup previous;
-		GetGroups(out previous, out IGroup selected, out IGroup next);
-		if(previous != null) {
-			previous.Select();
-			selectedIndex = 0;
-		}
-	}
-
-	void SelectGroupDown()
-	{
-		IGroup next;
-		GetGroups(out IGroup previous, out IGroup selected, out next);
-		if (next != null) {
-			next.Select();
-			selectedIndex = 0;
-		}
-	}
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
 }
